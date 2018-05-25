c1e7be95b9c740815b6fa922e15ff3b18466565d
Martin Cisneros <martin.cisneros@gmail.com>
2018-05-25 18:03:45 -0500

    nuevo login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
+import { Component, OnInit } from '@angular/core';
 import { IntroductionPage } from './../introduction/introduction';
 import { CarListPage } from './../car-list/car-list';
-import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams, IonicPage, LoadingController, ToastController } from 'ionic-angular';
 import { RecoveryPage } from '../recovery/recovery';
 import { RegistrerPage } from '../registrer/registrer';
@@ -17,21 +17,28 @@ import { ValidateEmail } from '../../app/validators/email.validator';
     selector: 'page-login',
     templateUrl: 'login.html',
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit {    
+    
     credentials = {} as usercreds;
+    
     constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, private storage: Storage, public authservice: AuthProvider, public loadingCtrl: LoadingController, public toastCtrl: ToastController) { }
-    //email: string;
-    //pass: string;
+    
+    email:string;
+    password:string;
+
     message: string;
     isEnabled: boolean;
     myForm: FormGroup;
-	password_type: string = 'password';
+    password_type: string = 'password';
+    validEmail;
+    validPass;
+    
     ngOnInit() {
         this.myForm = new FormGroup({
             user: new FormControl('', [Validators.required, ValidateEmail]),
             pass: new FormControl('', [Validators.required, Validators.minLength(7)]),
         });
-    }
+    }    
 
     onSubmit(form: FormGroup) {
         let toaster;
@@ -71,69 +78,122 @@ export class LoginPage implements OnInit {
     goCarList2 = () => {
         this.navCtrl.push(Login2Page, { animate: true });
     }
+
+    private emailChange(a) {
+        if (a.length == 0) {
+            this.validEmail = undefined;
+        } else { 
+            this.validateEmail();
+        }        
+        
+    }
+
+    private passChange(pass) {
+        if (pass.length == 0) {
+            this.validPass = undefined;
+        } else { 
+            this.validatePass();
+        }
+    }
+
+    private validateEmail(): boolean {
+        let email = this.email;
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let result = re.test(String(email).toLowerCase());
+        if (result === true) {
+            console.log('validateEmail', true);
+            this.validEmail = true;
+            return true;
+        } else {
+            console.log('validateEmail', false);
+            this.validEmail = false;
+            return  false;
+        }
+    }
+
+    private validatePass() {
+        let pass = this.password;
+        if (pass.length >= 6) {
+            this.validPass = true;
+            return true;
+        } else {
+            this.validPass = false;
+            return false;
+        }
+    }    
+
     goIntro = () => {
         let toaster;
         toaster = this.toastCtrl.create({
             duration: 3000,
             position: 'bottom'
         });
-        // toaster.dismiss();
-        console.log('goIntro');
+        let loader = this.loadingCtrl.create({
+            dismissOnPageChange: true,
+        });
+        loader.present();        
+
+        if (this.validatePass() === false) {
+            loader.dismiss();
+            toaster.setMessage('Indique un password válido');
+            toaster.present();	  
+            return;
+        } else if (this.validateEmail() === false) {
+            loader.dismiss();
+            toaster.setMessage('Indique un email válido');
+            toaster.present();	  
+            return;
+        }
 
         this.message = '';
         this.isEnabled = false;
 
-        let email = this.credentials.email,
-            pass = this.credentials.password;
+        let email = this.email,
+            pass = this.password;
 
         console.log({email, pass});
 
         if (email != undefined && pass != undefined) {
             email = email.trim();
-            pass = pass.trim();
-            if (this.validateEmail(email) !== true) {
-                toaster.setMessage('Indique un email válido');
-                toaster.present();	  
-                this.isEnabled = true;
-            } else {
+            pass = pass.trim();            
 
-                let string = `usuario=${email}&password=${pass}`,
-                    encodedString = btoa(string),
-                    url = `http://services.bunch.guru/WebService.asmx/Login?param=${encodedString}`;
+            let string = `usuario=${email}&password=${pass}`,
+                encodedString = btoa(string),
+                url = `http://services.bunch.guru/WebService.asmx/Login?param=${encodedString}`;
 
-                console.log({string, encodedString, url});
+            console.log({string, encodedString, url});
 
-                this.http.get(url)
-                    .map(res => res.json())
-                    .subscribe(data => {
+            this.http.get(url).map(res => res.json()).subscribe(data => {
 
-                        console.log({ data });
+                console.log({ data });
 
-                        switch (data.respuesta) {
-                            case 'true':
-                                localStorage.idContVend = data.idContVend;
-                                this.storage.set('name', data.idContVend);
-                                this.signin();
-                                break;
-                            case 'false':
-                                this.isEnabled = true;
-                                toaster.setMessage('Verifica tu contraseña');
-                                toaster.present();  
-                                break;
-                            case 'No existe usuario':
-                                this.isEnabled = true;
-                                toaster.setMessage('El usuario no existe');
-                                toaster.present();  
-                                break;
-                            default:
-                                this.isEnabled = true;
-                                toaster.setMessage('Hubo un error, intenta de nuevo');
-                                toaster.present();
-                        }
-                    }, err => {
-                        console.log("el usuario no existe");
-                    });
-            }
+                switch (data.respuesta) {
+                    case 'true':
+                        localStorage.idContVend = data.idContVend;
+                        this.storage.set('name', data.idContVend);
+                        this.signin();
+                        break;
+                    case 'false':
+                        this.isEnabled = true;
+                        loader.dismiss();
+                        toaster.setMessage('Verifica tu contraseña');
+                        toaster.present();  
+                        break;
+                    case 'No existe usuario':
+                        this.isEnabled = true;
+                        loader.dismiss();
+                        toaster.setMessage('El usuario no existe');
+                        toaster.present();  
+                        break;
+                    default:
+                        this.isEnabled = true;
+                        loader.dismiss();
+                        toaster.setMessage('Hubo un error, intenta de nuevo');
+                        toaster.present();
+                }
+            }, err => {
+                console.log("el usuario no existe");
+            });            
         } else {
             this.isEnabled = true;
             if (email == undefined) {
@@ -147,7 +207,8 @@ export class LoginPage implements OnInit {
     }
 
     signin() {
-        this.authservice.login(this.credentials).then((res: any) => {
+        let credentials = {email: this.email, password: this.password};
+        this.authservice.login(credentials).then((res: any) => {
             if (!res.code) {
                 this.navCtrl.push(HomePage, { animate: true });
             } else {
@@ -155,13 +216,7 @@ export class LoginPage implements OnInit {
                 console.error({res});
             }
         });
-    }
-
-    validateEmail(email: string): boolean {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
-
+    }    
 
     togglePasswordMode() {
         this.password_type = this.password_type === 'text' ? 'password' : 'text';
